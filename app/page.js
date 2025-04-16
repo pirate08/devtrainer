c01279b97a9f6bf874b83ef7a8e76bac39185657
Mayukh <mayukhdebgoswami82@gmail.com>
2025-04-16 16:36:20 +0530

    Initial project setup with Next.js and Tailwind

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='bg-black text-3xl text-white w-full h-screen flex justify-center items-center'>
+        Hello world!
+      </h1>
+    </div>
+  );
+}
